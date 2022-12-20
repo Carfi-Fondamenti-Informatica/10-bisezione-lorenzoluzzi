61629a29f8b1c52b00e5ae31e69eefcaedfbdc09
lorenzoluzzi <117952492+lorenzoluzzi@users.noreply.github.com>
2022-12-20 21:39:32 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,43 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 
-int main() {
-   return 0;
+void print (float x){
+    cout << int (x*10000)/10000.0;
+}
+
+float f(float a){
+    return (a*a*cos(a))+1;
+}
+
+bool verifica(float a, float b){
+    if(f(a)*f(b)>=0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+int main(){
+    float a,b;
+    do{
+        cout<<"inserire estremi"<<endl;
+        cin>>a>>b;
+    }while(verifica(a,b));
+    float x;
+    float err;
+    do{
+        x=(a+b)/2;
+        if(f(x)==0){
+            break;
+        } else if (!verifica(a, x)){
+            b = x;
+        }else{
+            a = x;
+        }
+        err = abs((b - a) / 2);
+    }while(err>=1e-6);
+    print (x);
+    return 0;
 }
